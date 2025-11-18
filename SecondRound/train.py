c01cae9ee4b195a15bcaced6b1f8ef6285701047
ManuelFas <manuelfasruiz@gmail.com>
2025-11-18 08:10:49 +0100

    Working in the models

---
+++ SecondRound/train.py
@@ -0,0 +1,29 @@
+import os
+import pickle
+import random
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+import pandas as pd
+import polars as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from matplotlib import pyplot as plt
+from schedulefree import RAdamScheduleFree
+from scipy.spatial.transform import Rotation as R
+from sklearn.metrics import f1_score
+from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import DataLoader, Dataset
+from torchinfo import summary
+
+
+def seed_everything(seed):
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.use_deterministic_algorithms(True)
